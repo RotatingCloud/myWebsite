0e16d5593cb32caca8d13da189c25cd26d15f8b3
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2023-03-20 15:48:16 -0700

    added functionality to contact me

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,7 +9,7 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-const hiddenElements = document.querySelectorAll('.container');
+const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
 document.getElementById('body').style.color = '#ffffff';
@@ -32,4 +32,52 @@ function formatPhoneNumber(value) {
     }
     return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3,6)}-${phoneNumber.slice(6, 9)}`;
 
+}
+
+function sendEmail() {
+    
+    var params = {
+        
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    }
+
+    if(isEmptyOrSpaces(params.name)) {
+
+        alert("youre not going to leave your name?");
+        return false;
+    
+    } else if(isEmptyOrSpaces(params.email)) {
+
+        alert("add an email so i can message you back!");
+        return false;
+    
+    } else if(isEmptyOrSpaces(params.message)) {
+
+        alert("say something funny");
+        return false;
+    }
+
+    const serviceID = "service_yev9lpt";
+    const templateID = "template_80f68u8";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(
+            res => {
+
+                console.log(res);
+                alert("message sent! :)");
+            }
+        )
+        .catch((err) => console.log(err));
+
+    
+    return false;
+}
+
+function isEmptyOrSpaces(str){
+    
+    return str === null || str.match(/^ *$/) !== null;
 }
