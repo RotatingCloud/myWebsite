1ac0f8153eb3c4182f022f97e9a1bc14f5c530c6
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2023-03-29 19:40:34 -0700

    .

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    fetch("/myWebsite/navbar/navbar.html")
+    fetch("../navbar/navbar.html")
     .then(response => response.text())
     .then(data => {
         document.body.insertAdjacentHTML("afterbegin", data);
