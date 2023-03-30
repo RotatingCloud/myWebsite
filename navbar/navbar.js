bdeccc913b48e2f11f4133ad6d2f95ab13a38531
RotatingCloud <113541115+RotatingCloud@users.noreply.github.com>
2023-03-30 08:50:18 -0700

    .

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,8 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    fetch('../myWebsite/navbar/navbar.html')
-    .then(response => response.text())
-    .then(data => {
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname;
+    const isIndexPage = currentPath.endsWith("index.html") || currentPath.endsWith("/");
+    const isInHtmlFolder = currentPath.includes("/html/");
+  
+    let navbarPath;
+  
+    if (isIndexPage) {
+      navbarPath = "navbar/navbar.html";
+    } else if (isInHtmlFolder) {
+      navbarPath = "../navbar/navbar.html";
+    } else {
+      navbarPath = "navbar/navbar.html";
+    }
+  
+    fetch(navbarPath)
+      .then((response) => response.text())
+      .then((data) => {
         document.body.insertAdjacentHTML("afterbegin", data);
-    });
-});
+      });
+  });
